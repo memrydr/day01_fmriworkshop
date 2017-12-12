28e23d04d4ae0bef9a313a67e697792a73163e33
memrydr <sappletonknapp@ucsd.edu>
2017-12-12 14:10:26 -0500

    prepro not working

--- prepro_sak.py
+++ prepro_sak.py
@@ -23,3 +23,46 @@ def main():
     prepro(basedir) #call prepro to do cool things
     
 main()
+input=glob.glob('/Users/saraappleton-knapp/Documents/CHEAR/fMRI workshop/data1/sub-*/func/sub-*bart*.nii.gz')
+print(input[0:10])
+
+x=input[0]
+print('my path is '+x)
+y=x.split('/')
+print (y)
+sub=y[7]
+
+#whatcomp=y[2]
+#sub=y[5]
+#print sub
+
+sub=input[0].split('/')[7]
+print(sub)
+
+subtask=input[1].split('/')[9].split('.')[0]
+#subtask=subtask.strip('.nii.gz')
+print(subtask)
+
+output=subtask+'_brain'
+print(output)
+os.system('bet %s %s -F'%(x, output))
+
+basedir='/Users/saraappleton-knapp/Documents/CHEAR/fMRI_workshop/data1'
+path=os.path.join(basedir,'sub-*','func','sub-*bart*.nii.gz')
+print(path)
+input=glob.glob(path)
+len(input[1:5])
+print(input)
+#os.path.exists(basedir)
+
+def prepro(basedir):
+    for item in glob.glob(os.path.join(basedir,'sub-*','func','sub-*bart*.nii.gz')):
+        input=item
+        output_path=item.strip('.nii.gz')
+        output=output_path+('_brain')
+        os.system("/usr/local/fsl/bin/bet %s %s -F"%(input, output))
+        pdb.set_trace()
+def main():
+    basedir='/Users/saraappleton-knapp/Documents/CHEAR/fMRI_workshop/data1'
+    prepro(basedir)
+main()
